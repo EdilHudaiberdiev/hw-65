55f4b1fdf0392ac4f2c465085f77fecf50d62b36
Edil Hudaiberdiev <hudaiberdiev.ed@gmail.com>
2023-12-07 00:10:58 +0600

    home additions

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
 import './App.css'
 import {Route, Routes} from "react-router-dom";
 import Toolbar from "./Components/Toolbar/Toolbar";
+import Home from "./Containers/Home/Home";
 
 const App = () => {
 
-  return (
-      <>
-          <header>
-              <Toolbar/>
-          </header>
-          <main className="container pt-5">
-              <Routes>
-                  <Route/>
-                  <Route/>
-              </Routes>
-          </main>
-      </>
-  )
+    return (
+        <>
+            <header>
+                <Toolbar/>
+            </header>
+            <main className="container pt-5">
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/:namePage" element={<Home/>}/>
+                </Routes>
+            </main>
+        </>
+    )
 };
 
 export default App
--- src/Containers/Home/Home.tsx
+++ src/Containers/Home/Home.tsx
@@ -1,6 +1,52 @@
+import {useCallback, useEffect, useState} from "react";
+import axiosApi from "../../axiosApi";
+import {useParams} from "react-router-dom";
+import Spinner from "../../Components/UI/Spinner/Spinner";
 
 const Home = () => {
+    const [pageContent, setContentPage] = useState<IPage>();
+    const [loading, setLoading] = useState(false);
+    const params = useParams();
 
+    const fetchData = useCallback( async (url: string) => {
+
+        setLoading(true);
+        const response = await axiosApi.get(url);
+        const page: IPage = response.data;
+
+        if (response.data !== null) {
+            setContentPage(page);
+        }
+        setLoading(false);
+    }, []);
+
+    useEffect(() => {
+        if (params.namePage !== undefined) {
+            void fetchData(`/pages/${params.namePage}.json`);
+        } else {
+            void fetchData(`/pages/home.json`);
+        }
+
+    }, [fetchData, params]);
+
+    return (
+        <div>
+            {loading ? <Spinner/> :
+                <>
+                    {pageContent ?
+                        <div>
+                            <h4>{pageContent.title}</h4>
+                            <p>{pageContent.text}</p>
+                        </div>
+                        :
+                        <p>No content</p>
+                    }
+                </>
+
+            }
+
+        </div>
+    );
 };
 
 export default Home;
